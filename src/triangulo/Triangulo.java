b94807a61b75bd8ce4c8aa64cef7d3f0a805dfd0
Prof. Celso <celsofurtado@terra.com.br>
2021-08-27 15:57:22 -0300

    Aula 27-08-2021 - DS1T

---
+++ src/triangulo/Triangulo.java
@@ -0,0 +1,42 @@
+package triangulo;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.print("Qual a medida do lado A?");
+		a = leitor.nextInt();
+		
+		System.out.print("Qual a medida do lado B?");
+		b = leitor.nextInt();
+		
+		System.out.print("Qual a medida do lado C?");
+		c = leitor.nextInt();
+		
+		leitor.close();
+		
+		// Verificar se é um triângulo
+		if (b + a > c && c + b > a && c + a > b) {
+			
+			if (a == b && b == c && c == a) {
+				System.out.println("EQUILÁTERO");
+			} else if(a != b && b != c && c != a) {
+				System.out.println("ESCALENO");
+			} else {
+				System.out.println("ISÓSCELES");
+			}
+			
+			
+		} else {
+			System.out.println("Sinto muito, não é um triângulo!");
+		}
+
+	}
+
+}
